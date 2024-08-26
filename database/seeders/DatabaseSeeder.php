243e523fcb823dc01f809764cbc82663f6c687d1
marcoantoniosz <marcoantonio99d@gmail.com>
2024-08-26 19:01:48 -0300

    :seedling: Seeding: add seed for Category and Product tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        //Category Seeds
+
+        Category::factory()->create([
+            'name' => 'Rock',
+        ]);
+        Category::factory()->create([
+            'name' => 'Alternative'
+        ]);
+        Category::factory()->create([
+            'name' => 'Pop'
+        ]);
+        Category::factory()->create([
+            'name' => 'Electronic'
+        ]);
+
+        //Category Seeds
+
+        //Product Seeds
+
+        Product::factory()->create([
+            'name' => 'OK Computer - Radiohead',
+            'category_id' => 1,
+            'price' => 9.99,
+            'stock' => 10,
+        ]);
+        Product::factory()->create([
+            'name' => 'Vespertine - Bjork',
+            'category_id' => 2,
+            'price' => 14.99,
+            'stock' => 25,
+        ]);
+        Product::factory()->create([
+            'name' => 'Melodrama - Lorde',
+            'category_id' => 3,
+            'price' => 14.99,
+            'stock' => 5,
+        ]);
+        Product::factory()->create([
+            'name' => '7G - A. G. Cook',
+            'category_id' => 4,
+            'price' => 20.99,
+            'stock' => 15,
         ]);
     }
 }
